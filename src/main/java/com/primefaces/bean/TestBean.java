1a0bbbaafc8b9d8d937fcae24f2b0bc7eb5561e7
Mohanapriya Palanisamy <m.palanisamy@perficient.com>
2017-03-29 18:04:35 +0530

    Initial Commit

---
+++ src/main/java/com/primefaces/bean/TestBean.java
@@ -0,0 +1,33 @@
+package com.primefaces.bean;
+
+public class TestBean {
+	private String username="testUserName";
+	private String password;
+	private String passwordConfirm;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+
+}
